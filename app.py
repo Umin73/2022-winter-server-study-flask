beee41abb69ccd8440fee3e90bd3f4a9fc9081ec
Umin73 <trillionumin@gmail.com>
2023-01-27 13:48:42 +0900

    commit

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/id/<int:id>', methods=['GET'])
-def get(id):
-    return {"message": ""}
+def get_api(id):
+ if id >= 5000: return {"result" : True}
+ else: return {"result" : False}
 
 @app.route('/id', methods=['POST'])
-def post():
-    return {'name': ''}
+def get_post():
+ data = request.get_json()
+ return jsonify({'name' : data['name']})
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
